465331fed0bdca7f676f90f956bbea17f8750f63
tuan3110 <chodai23590@gmail.com>
2025-01-02 19:08:11 +0700

    feat: create layout

--- src/router.jsx
+++ src/router.jsx
@@ -1,17 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "@/pages/HomePage";
 import AdminPage from "@/pages/AdminPage";
+import Layout from "@/components/Layout";
 
 // eslint-disable-next-line react-refresh/only-export-components
-export default createBrowserRouter(
-    [
-        {
-            path: "/",
-            element: <HomePage />
-        },
-        {
-            path: "/admin",
-            element: <AdminPage />
-        }
-    ]
-)
+export default createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/admin",
+        element: <AdminPage />,
+      },
+    ],
+  },
+]);
